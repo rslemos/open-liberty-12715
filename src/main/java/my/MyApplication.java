868e339f1c45e3b85c27b107bb2bfaeccfe3691b
R. Lemos <rslemos@gmail.com>
2020-06-19 13:13:09 -0300

    [INFO] === REPORT ===
    [INFO] securityContext1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@5b906af6	SUCCESS
    [INFO] securityContext2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@500a92b1	SUCCESS
    [INFO] securityContext3	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@5688ba70	SUCCESS
    [INFO] application1.securityContext1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] application2.securityContext1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] application3.securityContext1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] application1.securityContext2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] application2.securityContext2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] application3.securityContext2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] securityContext1FromApplication1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] securityContext1FromApplication2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] securityContext1FromApplication3	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] securityContext2FromApplication1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] securityContext2FromApplication2	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] securityContext2FromApplication3	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] class.securityContext1FromApplication1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] class.securityContext1FromApplication3	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@6364690d	SUCCESS
    [INFO] class.securityContext2FromApplication1	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] class.securityContext2FromApplication3	org.apache.cxf.jaxrs.impl.tl.ThreadLocalSecurityContext@3bfb684d	SUCCESS
    [INFO] application1.securityContext3	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] application2.securityContext3	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] application3.securityContext3	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] securityContext3FromApplication1	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] securityContext3FromApplication2	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] securityContext3FromApplication3	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] class.securityContext3FromApplication1	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] class.securityContext3FromApplication3	null	FAILURE: NullPointerException at my.MyContextResolver$1.report(MyContextResolver.java:138)
    [INFO] === END OF REPORT ===

--- src/main/java/my/MyApplication.java
+++ src/main/java/my/MyApplication.java
@@ -1,8 +1,36 @@
 package my;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 @ApplicationPath("/myapp")
 public class MyApplication extends Application {
+  private SecurityContext securityContext1;
+  @Context
+  private SecurityContext securityContext2;
+  private SecurityContext securityContext3 = null;
+
+  // Resource classes with singleton scope can only have ServletConfig or ServletContext instances injected through their constructors
+  // public MyApplication(@Context SecurityContext securityContext) {
+  //   securityContext3 = securityContext;
+  // }
+
+  @Context
+  public void setSecurityContext(SecurityContext securityContext) {
+    securityContext1 = securityContext;
+  }
+
+  @Override
+  public Map<String, Object> getProperties() {
+    Map<String, Object> properties = new HashMap<>();
+    properties.put("securityContext1", securityContext1);
+    properties.put("securityContext2", securityContext2);
+    // properties.put("securityContext3", securityContext3);
+    return properties;
+  }
 }
